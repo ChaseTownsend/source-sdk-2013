6b4f9713543ef4166dd9f594f83b91d904f74d7b
The Fatcat <townsend.chase128@gmail.com>
2025-08-10 21:15:09 -0500

    Reimplement panic_attack

--- src/game/shared/tf/tf_weaponbase.cpp
+++ src/game/shared/tf/tf_weaponbase.cpp
@@ -2258,18 +2258,22 @@ void CTFWeaponBase::SetReloadTimer( float flReloadTime )
 	CALL_ATTRIB_HOOK_FLOAT_ON_OTHER( pPlayer, flReloadTime, hwn_mult_reload_time );
 
 	//int iPanicAttack = 0;
-	//CALL_ATTRIB_HOOK_INT( iPanicAttack, panic_attack );
-	//if ( iPanicAttack ) 
-	//{
-	//	if ( pPlayer->GetHealth() < pPlayer->GetMaxHealth() * 0.33f )
-	//	{
-	//		flReloadTime *= 0.3f;
-	//	}
-	//	else if ( pPlayer->GetHealth() < pPlayer->GetMaxHealth() * 0.66f )
-	//	{
-	//		flReloadTime *= 0.6f;
-	//	}
-	//}
+	int iPanicAttack = 0;
+	CALL_ATTRIB_HOOK_INT( iPanicAttack, panic_attack );
+	if ( iPanicAttack ) 
+	{
+		// Maybe Not needed but, just in case
+		if ( pPlayer->GetHealth() == 1 )
+		{ 
+			flReloadTime *= 0.3f; 
+		}
+		else if ( pPlayer->GetHealth() < pPlayer->GetMaxHealth() )
+		{
+			float healthpercent = (float)pPlayer->GetHealth() / (float)pPlayer->GetMaxHealth();
+			// flReloadTime *= RemapValClamped(pPlayer->HealthFraction(), 0.0f, 1.0f, 0.3f, 1.0f);
+			flReloadTime *= RemapValClamped(healthpercent, 0.0f, 1.0f, 0.3f, 1.0f);
+		}
+	}
 
 	// Haste Powerup Rune adds multiplier to reload time.
 	if ( pPlayer->m_Shared.GetCarryingRuneType() == RUNE_HASTE )
